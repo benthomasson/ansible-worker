1fc7c08fe9aaadab784b2836d4939b415b4b2329
Ben Thomasson <bthomass@redhat.com>
2018-11-16 10:16:04 -0500

    Adds messages, worker, and worker_fsm from ansible_worker_poller

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 'gevent_fsm', 'ansible_runner', 'ansible']
 
 setup_requirements = ['pytest-runner', ]
 
